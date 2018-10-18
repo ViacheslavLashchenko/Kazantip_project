687b0c5baec6adc0630f08440f9f523f92534e4a
kazantip <gavrilcbwd78@gmail.com>
2018-10-18 17:20:14 +0300

    Начало верстки

--- index.php
+++ index.php
@@ -1,16 +1,111 @@
 ﻿<!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Wedding Dresses London - A huge range of Bridal Gowns</title>
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
-		</script>
-		<link  rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
-		</script>
+	<meta charset="UTF-8">
+	   <meta  name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
+	<nav class="navbar navbar-expand-lg navbar-light ">
+		<div class="container">
+		  	<a class="navbar-brand" href="#">
+				<img src="images/London-Bride-UK.png" alt="London-Bride-UK">
+			</a>
+		  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbar1" aria-expanded="false" aria-label="Toggle navigation">
+		    	<span class="navbar-toggler-icon"></span>
+		  	</button>
 
+		  	<div class="collapse navbar-collapse" id="navbar1">
+		    	<ul class="navbar-nav">
+		      		<li class="nav-item active">
+		        		<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      		</li>
+				    <li class="nav-item ml-3">
+				    	<a class="nav-link" href="#">Designers</a>
+				    </li>
+				    <li class="nav-item ml-3">
+				    	<a class="nav-link" href="#">Sale Dresses</a>
+				    </li>
+				    <li class="nav-item ml-3">
+				    	<a class="nav-link" href="#">Blog</a>
+				    </li>
+				    <li class="nav-item ml-3">
+				    	<a class="nav-link" href="#">Contact</a>
+				    </li>
+		    	</ul>
+		  	</div>
+		</div>
+	</nav>
+
+
+	<div style="clear: both"></div>
+
+	<div id="carousel" class="carousel slide" data-ride="carousel">
+	  <div class="carousel-inner">
+	    <div class="carousel-item active">
+	        <img class="img-fluid" src="images/Bridal-Dresses-London-2.png" alt="...">
+	        <div class="carousel-caption centered">
+	<span class="slide-title">Dresses Available Today</span>
+	<small>
+		<h3>Dresses Available To Buy Immediately in sizes 4-26</h3>
+		<h4>Call 020 7703 6796 To Book Your Appointment</h4>
+	</small>
+	      	</div>
+	    </div>
+	    <div class="carousel-item">
+	       	<img class="img-fluid" src="images/Bridal-Dresses-London-2.png" alt="...">
+	       	<div class="carousel-caption centered">
+				<span class="slide-title">Dresses Available Today</span>
+				<small>
+					<h3>Dresses Available To Buy Immediately in sizes 4-26</h3>
+					<h4>Call 020 7703 6796 To Book Your Appointment</h4>
+				</small>
+	      	</div>
+	    </div>
+	    <div class="carousel-item">
+	        <img class="img-fluid" src="images/Bridal-Dresses-London-2.png" alt="...">
+	         <div class="carousel-caption centered">
+	<span class="slide-title">Dresses Available Today</span>
+	<small>
+		<h3>Dresses Available To Buy Immediately in sizes 4-26</h3>
+		<h4>Call 020 7703 6796 To Book Your Appointment</h4>
+	</small>
+	      	</div>
+	    </div>
+	</div>
+</div>
+	 <!-- Элементы управления
+	         <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Предыдущий</span>
+	  </a>
+	  <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Следующий</span>
+	  </a>
+	  </div>
+	</div>
+  Controls
+<a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a> -->  
+
+
+
+
+
+<script src="https://unpkg.com/popper.js/dist/umd/popper.min.js"></script> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
 </body>
 </html>
