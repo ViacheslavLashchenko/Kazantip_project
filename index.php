077505745f5b0a77a6d21a1f413acee24c587ea5
kazantip <gavrilcbwd78@gmail.com>
2018-10-19 10:45:43 +0300

    добавка всех файлов

--- index.php
+++ index.php
@@ -9,94 +9,83 @@
 	<link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-	<nav class="navbar navbar-expand-lg navbar-light ">
-		<div class="container">
-		  	<a class="navbar-brand" href="#">
-				<img src="images/London-Bride-UK.png" alt="London-Bride-UK">
-			</a>
-		  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbar1" aria-expanded="false" aria-label="Toggle navigation">
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-light ">
+			<div class="container">
+		  		<a class="navbar-brand" href="#">
+					<img src="images/London-Bride-UK.png" alt="London-Bride-UK">
+				</a>
+		  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbar1" aria-expanded="false" aria-label="Toggle navigation">
 		    	<span class="navbar-toggler-icon"></span>
-		  	</button>
+		  		</button>
 
-		  	<div class="collapse navbar-collapse" id="navbar1">
-		    	<ul class="navbar-nav">
-		      		<li class="nav-item active">
-		        		<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-		      		</li>
-				    <li class="nav-item ml-3">
+		  		<div class="collapse navbar-collapse" id="navbar1">
+		    		<ul class="navbar-nav">
+		      			<li class="nav-item active">
+		        			<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      			</li>
+				    	<li class="nav-item ml-3">
 				    	<a class="nav-link" href="#">Designers</a>
-				    </li>
-				    <li class="nav-item ml-3">
+				   		</li>
+				   		<li class="nav-item ml-3">
 				    	<a class="nav-link" href="#">Sale Dresses</a>
-				    </li>
-				    <li class="nav-item ml-3">
+				    	</li>
+				    	<li class="nav-item ml-3">
 				    	<a class="nav-link" href="#">Blog</a>
-				    </li>
-				    <li class="nav-item ml-3">
+				    	</li>
+				    	<li class="nav-item ml-3">
 				    	<a class="nav-link" href="#">Contact</a>
-				    </li>
-		    	</ul>
-		  	</div>
-		</div>
-	</nav>
+				    	</li>
+		    		</ul>
+		  		</div>
+			</div>
+		</nav>
 
 
-	<div style="clear: both"></div>
+		<!-- <div style="clear: both"></div> -->
 
-	<div id="carousel" class="carousel slide" data-ride="carousel">
-	  <div class="carousel-inner">
-	    <div class="carousel-item active">
-	        <img class="img-fluid" src="images/Bridal-Dresses-London-2.png" alt="...">
+		<div id="carousel" class="carousel slide" data-ride="carousel">
+	  	<div class="carousel-inner">
+	    	<div class="carousel-item active">
+	        	<img class="img-fluid" src="images/Bridal-Dresses-London-2.png" alt="...">
 	        <div class="carousel-caption centered">
-	<span class="slide-title">Dresses Available Today</span>
-	<small>
-		<h3>Dresses Available To Buy Immediately in sizes 4-26</h3>
-		<h4>Call 020 7703 6796 To Book Your Appointment</h4>
-	</small>
+		<span class="slide-title">Dresses Available Today</span>
+		<small>
+			<h3>Dresses Available To Buy Immediately in sizes 4-26</h3>
+			<h4>Call 020 7703 6796 To Book Your Appointment</h4>
+		</small>
 	      	</div>
-	    </div>
-	    <div class="carousel-item">
-	       	<img class="img-fluid" src="images/Bridal-Dresses-London-2.png" alt="...">
-	       	<div class="carousel-caption centered">
-				<span class="slide-title">Dresses Available Today</span>
-				<small>
-					<h3>Dresses Available To Buy Immediately in sizes 4-26</h3>
-					<h4>Call 020 7703 6796 To Book Your Appointment</h4>
-				</small>
-	      	</div>
-	    </div>
-	    <div class="carousel-item">
-	        <img class="img-fluid" src="images/Bridal-Dresses-London-2.png" alt="...">
-	         <div class="carousel-caption centered">
-	<span class="slide-title">Dresses Available Today</span>
-	<small>
-		<h3>Dresses Available To Buy Immediately in sizes 4-26</h3>
-		<h4>Call 020 7703 6796 To Book Your Appointment</h4>
-	</small>
-	      	</div>
-	    </div>
+	    	</div>
+	    	<div class="carousel-item">
+	       		<img class="img-fluid" src="images/Bridal-Dresses-London-2.png" alt="...">
+	       		<div class="carousel-caption centered">
+					<span class="slide-title">Dresses Available Today</span>
+					<small>
+						<h3>Dresses Available To Buy Immediately in sizes 4-26</h3>
+						<h4>Call 020 7703 6796 To Book Your Appointment</h4>
+					</small>
+	      		</div>
+	    	</div>
+	    	<div class="carousel-item">
+	        	<img class="img-fluid " src="images/Bridal-Dresses-London-2.png" alt="...">
+	         	<div class="carousel-caption centered">
+					<span class="slide-title">Dresses Available Today</span>
+					<small>
+						<h3>Dresses Available To Buy Immediately in sizes 4-26</h3>
+						<h4>Call 020 7703 6796 To Book Your Appointment</h4>
+					</small>
+	      		</div>
+	    	</div>
+		</div>
+		</div>
+	</header>
+	<div class="content">
+		<div class="row align-items-start justify-content-center">
+			<div class="col-lg-12 dbline">
+				<span><a class="about" href="#">About</a></span>
+			</div>
+		</div>
 	</div>
-</div>
-	 <!-- Элементы управления
-	         <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-	    <span class="sr-only">Предыдущий</span>
-	  </a>
-	  <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-	    <span class="sr-only">Следующий</span>
-	  </a>
-	  </div>
-	</div>
-  Controls
-<a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a> -->  
 
 
 
