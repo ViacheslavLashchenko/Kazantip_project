b53c7bba3c213b849b7369805cb5536556a77924
kazantip <gavrilcbwd78@gmail.com>
2018-10-30 21:59:23 +0200

    изменил шапку с логотипом, и кнопку about в моб. виде

--- index.php
+++ index.php
@@ -1,115 +1,108 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	   <meta  name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>Document</title>
-	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
+
 <body>
-
-	<header>
-		<nav class="navbar navbar-expand-lg navbar-light ">
-			<div class="container">
-		  		<a class="navbar-brand" href="#">
-					<img src="images/London-Bride-UK.png" alt="London-Bride-UK">
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light ">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+					<img class="img-fluid" src="images/London-Bride-UK.png" alt="London-Bride-UK">
 				</a>
-		  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbar1" aria-expanded="false" aria-label="Toggle navigation">
-		    	<span class="navbar-toggler-icon"></span>
-		  		</button>
-
-		  		<div class="collapse navbar-collapse" id="navbar1">
-		    		<ul class="navbar-nav">
-		      			<li class="nav-item active">
-		        			<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-		      			</li>
-				    	<li class="nav-item ml-3">
-				    	<a class="nav-link" href="#">Designers</a>
-				   		</li>
-				   		<li class="nav-item ml-3">
-				    	<a class="nav-link" href="#">Sale Dresses</a>
-				    	</li>
-				    	<li class="nav-item ml-3">
-				    	<a class="nav-link" href="#">Blog</a>
-				    	</li>
-				    	<li class="nav-item ml-3">
-				    	<a class="nav-link" href="#">Contact</a>
-				    	</li>
-		    		</ul>
-		  		</div>
-			</div>
-		</nav>
-
-
-		<!-- <div style="clear: both"></div> -->
-
-		<div id="carousel" class="carousel slide" data-ride="carousel">
-	  	<div class="carousel-inner">
-	    	<div class="carousel-item active">
-	        	<img class="img-fluid" src="images/Bridal-Dresses-London-2.png" alt="...">
-	        <div class="carousel-caption centered">
-		<span class="slide-title">Dresses Available Today</span>
-		<small>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbar1" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbar1">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item ml-3">
+                            <a class="nav-link" href="#">Designers</a>
+                        </li>
+                        <li class="nav-item ml-3">
+                            <a class="nav-link" href="#">Sale Dresses</a>
+                        </li>
+                        <li class="nav-item ml-3">
+                            <a class="nav-link" href="#">Blog</a>
+                        </li>
+                        <li class="nav-item ml-3">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+        </div>
+               	</div>
+            </div>
+        </nav>
+        <!-- <div style="clear: both"></div> -->
+        <div id="carousel" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="img-fluid" src="images/Bridal-Dresses-London-2.png" alt="...">
+                    <div class="carousel-caption centered">
+                        <span class="slide-title">Dresses Available Today</span>
+                        <small>
 			<h3>Dresses Available To Buy Immediately in sizes 4-26</h3>
 			<h4>Call 020 7703 6796 To Book Your Appointment</h4>
 		</small>
-	      	</div>
-	    	</div>
-	    	<div class="carousel-item">
-	       		<img class="img-fluid" src="images/Bridal-Dresses-London-2.png" alt="...">
-	       		<div class="carousel-caption centered">
-					<span class="slide-title">Dresses Available Today</span>
-					<small>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="img-fluid" src="images/Bridal-Dresses-London-2.png" alt="...">
+                    <div class="carousel-caption centered">
+                        <span class="slide-title">Dresses Available Today</span>
+                        <small>
 						<h3>Dresses Available To Buy Immediately in sizes 4-26</h3>
 						<h4>Call 020 7703 6796 To Book Your Appointment</h4>
 					</small>
-	      		</div>
-	    	</div>
-	    	<div class="carousel-item">
-	        	<img class="img-fluid " src="images/Bridal-Dresses-London-2.png" alt="...">
-	         	<div class="carousel-caption centered">
-					<span class="slide-title">Dresses Available Today</span>
-					<small>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="img-fluid " src="images/Bridal-Dresses-London-2.png" alt="...">
+                    <div class="carousel-caption centered">
+                        <span class="slide-title">Dresses Available Today</span>
+                        <small>
 						<h3>Dresses Available To Buy Immediately in sizes 4-26</h3>
 						<h4>Call 020 7703 6796 To Book Your Appointment</h4>
 					</small>
-	      		</div>
-	    	</div>
-		</div>
-		</div>
-	</header>
-	<div class="container-fluid">
-		<div class="row justify-content-center about">
-			<div class="col-lg-auto my-auto shrink">
-				<a href="#"><button type="button" onclick="toggle('#menu-1');" class="btn btn-default">About</button></a>
-			</div>
-		</div>
-		<div class="row justify-content-center">
-			<div id="menu-1" class="col-lg-12">
-					<h1>Wedding Dresses London</h1>
-					<p><strong>We are one of London’s leading bridal shops with over 35 years of expertise and knowledge in the bridal profession.</strong></p>
-					<p><strong>We are one of London’s leading bridal shops with over 35 years of expertise and knowledge in the bridal profession.</strong></p>
-					<p>When you visit our store you will be helped by our friendly, professional team of bridal consultants who will assist you in the choice of your gown. We offer an in-house alteration service where you will have an appointment to ensure your gown fits perfectly and makes you look and feel sensational for your special day. We also have a wide range of gowns that are available to buy off the rail and a large selection of sizes (including plus sizes) are always in stock. Private fittings are available on request.</p>
-					<p>If you have any questions, please feel free to give us a call on 020 7703 6796, alternatively, you can book an appointment online by filling in the form at the bottom of the page.</p>
-					<p><strong>We are one of London’s leading bridal shops with over 35 years of expertise and knowledge in the bridal profession.</strong></p>
-					<p>We stock a huge selection of designer wedding gowns featuring both contemporary and classic designs to ensure that you will be able to find the perfect dress for your special day.</p>
-					<p>When you visit our store you will be helped by our friendly, professional team of bridal consultants who will assist you in the choice of your gown. We offer an in-house alteration service where you will have an appointment to ensure your gown fits perfectly and makes you look and feel sensational for your special day. We also have a wide range of gowns that are available to buy off the rail and a large selection of sizes (including plus sizes) are always in stock. Private fittings are available on request.</p>
-					<p>If you have any questions, please feel free to give us a call on 020 7703 6796, alternatively, you can book an appointment online by filling in the form at the bottom of the page.</p>
-				</div>
-		</div>
-				
-			</div>
-
-
-
-
-
-<script src="https://unpkg.com/popper.js/dist/umd/popper.min.js"></script> 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/main.js"></script>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container-fluid">
+        <div class="row justify-content-center about">
+            <div class="col-auto my-auto shrink">
+                <button type="button" onclick="toggle('#menu-1');" class="btn btn-default">About</button>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div id="menu-1" class="col-lg-12">
+                <h1>Wedding Dresses London</h1>
+                <p><strong>We are one of London’s leading bridal shops with over 35 years of expertise and knowledge in the bridal profession.</strong></p>
+                <p><strong>We are one of London’s leading bridal shops with over 35 years of expertise and knowledge in the bridal profession.</strong></p>
+                <p>When you visit our store you will be helped by our friendly, professional team of bridal consultants who will assist you in the choice of your gown. We offer an in-house alteration service where you will have an appointment to ensure your gown fits perfectly and makes you look and feel sensational for your special day. We also have a wide range of gowns that are available to buy off the rail and a large selection of sizes (including plus sizes) are always in stock. Private fittings are available on request.</p>
+                <p>If you have any questions, please feel free to give us a call on 020 7703 6796, alternatively, you can book an appointment online by filling in the form at the bottom of the page.</p>
+                <p><strong>We are one of London’s leading bridal shops with over 35 years of expertise and knowledge in the bridal profession.</strong></p>
+                <p>We stock a huge selection of designer wedding gowns featuring both contemporary and classic designs to ensure that you will be able to find the perfect dress for your special day.</p>
+                <p>When you visit our store you will be helped by our friendly, professional team of bridal consultants who will assist you in the choice of your gown. We offer an in-house alteration service where you will have an appointment to ensure your gown fits perfectly and makes you look and feel sensational for your special day. We also have a wide range of gowns that are available to buy off the rail and a large selection of sizes (including plus sizes) are always in stock. Private fittings are available on request.</p>
+                <p>If you have any questions, please feel free to give us a call on 020 7703 6796, alternatively, you can book an appointment online by filling in the form at the bottom of the page.</p>
+            </div>
+        </div>
+    </div>
+    <script src="https://unpkg.com/popper.js/dist/umd/popper.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
 </body>
+
 </html>
